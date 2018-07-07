04b2af3d34adfd8935d4e8d09bcb0d0aa9e478e9
vmetric <armada2526pbem@gmail.com>
2018-07-06 23:36:24 -0600

    Added delete script, initial creation of new sort script
    Added, cleaned up, and prepared the delete script for integration w/ C# program. Added a sort.bat to be used eventually as an automatic sorter (currently empty).

--- Scripts/delete.bat
+++ Scripts/delete.bat
@@ -6,12 +6,11 @@ REM Ensure script is "complete"
 REM Add documentation/commentary
 REM ---- END TODO : BEGIN SCRIPT ----
 
-set fromDir=%1
-set toDir=%2
-set filetype=%3
+set /p fromDir=%1
+set /p filetype=%2
 
 set fromDirPlusFiletype=%fromDir%*%filetype%
 
 set timesRan=0
-for /R "%fromDir%" %%i in (*%filetype%) do move "%%i" "%toDir%" >nul && set /a timesRan = timesRan + 1
-echo Successfully moved %timesRan% files
+for /R "%fromDir%"  %%i in (*%filetype%) do del "%%i" && set /a timesRan = timesRan + 1
+echo Successfully deleted %timesRan% files.
---
+++ Scripts/sort.bat
@@ -0,0 +1,2 @@
+REM Created 7/6/2018 by Matthew Putnam
+REM Purpose is to automatically sort files into appropriate folders.
