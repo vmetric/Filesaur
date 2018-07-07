d44437eadf5fe3f14028a5cc068ec87b3ddda8e6
vmetric <armada2526pbem@gmail.com>
2018-07-06 19:34:03 -0600

    Created C# Program; modified move script
    Initial creation of the C# program (Visual Studio 2017 WPF App), heavily modified/adjusted the move script, preparing it use w/ program.

--- Scripts/move.bat
+++ Scripts/move.bat
@@ -1,5 +1,19 @@
 REM Created 7/6/2018 by Matthew Putnam.
 
 REM ---- TODO ---- 
-REM Ensure a C# program can easily interact with and use this script.
+REM Ensure this script works w/ C# program
+REM 
+REM Add documentation/commentary
 REM ---- END TODO : BEGIN SCRIPT ----
+
+
+
+set fromDir=%1
+set toDir=%2
+set filetype=%3
+
+set fromDirPlusFiletype=%fromDir%*%filetype%
+
+set timesRan=0
+for /R "%fromDir%" %%i in (*%filetype%) do move "%%i" "%toDir%" >nul && set /a timesRan = timesRan + 1
+echo Successfully moved %timesRan% files
