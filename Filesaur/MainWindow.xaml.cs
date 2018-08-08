66c52a766cd1363465d241b7305a67784624cc98
Matt <40653288+vmetric@users.noreply.github.com>
2018-08-08 17:14:59 -0600

    Slight polish to the compacting

--- Filesaur/MainWindow.xaml.cs
+++ Filesaur/MainWindow.xaml.cs
@@ -18,8 +18,15 @@ namespace Filesaur
         {
             InitializeComponent();
         }
-
-        
+        // Methods to make changing control visibility a bit more shorthand.
+        public void hideControl(Control control)
+        {
+            control.Visibility = Visibility.Hidden;
+        }
+        public void showControl(Control control)
+        {
+            control.Visibility = Visibility.Visible;
+        }
 
         // Method to start a command prompt window, executing "operationToExecute"
         public void StartCMD(int operationToExecute)
